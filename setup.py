c4d8f365fa5e2203d47bdcde874d98a23df6a627
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-11-16 11:54:09 +0100

    Use setup.py instead
    Poetry is overkill when we only have one dependecy

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+readme = ''
+
+setup(
+    long_description=readme,
+    name='vip-ipykernel',
+    version='0.1.0',
+    python_requires='==3.*,>=3.6.0',
+    author='Robert Rosca',
+    author_email='32569096+RobertRosca@users.noreply.github.com',
+    packages=['vip_ipykernel'],
+    package_dir={"": "src"},
+    package_data={},
+    install_requires=['jupyter-client==6.*,>=6.1.7'],
+    extras_require={"dev": ["pytest==5.*,>=5.2.0"]},
+)
