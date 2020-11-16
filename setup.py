c53591bf351dd3c5e83e6838d07518f8dc17cfbe
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-11-16 13:27:31 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -12,6 +12,14 @@ setup(
     packages=['vip_ipykernel'],
     package_dir={"": "src"},
     package_data={},
-    install_requires=['jupyter-client==6.*,>=6.1.7'],
-    extras_require={"dev": ["pytest==5.*,>=5.2.0"]},
+    install_requires=[
+        'jupyter-client==6.*,>=6.1.7',
+    ],
+    extras_require={
+        'test': [
+            'pytest==5.*,>=5.2.0',
+            'nose==1.*,>=1.3.7',
+            'ipykernel==5.*,>=5.3.4'
+        ],
+    },
 )
