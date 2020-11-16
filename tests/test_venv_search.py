7fd00aea41539ca76005854253a5cd94069ceeb1
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-11-16 15:33:39 +0100

    Run test with shutil.which instaed of sys.executable

--- tests/test_venv_search.py
+++ tests/test_venv_search.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import shutil
 from pathlib import Path
 
 from vip_ipykernel.vip_ipykernel_launcher import venv_search
@@ -20,4 +20,4 @@ def test_venv_search(tmp_path):
 def test_venv_search_missing(tmp_path):
     os.chdir(tmp_path)
 
-    assert Path(sys.executable) == venv_search()
+    assert Path(shutil.which("python3")).resolve() == venv_search()
