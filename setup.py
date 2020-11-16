47358fab1caba7872249390049d1c563b41192c0
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-11-16 16:44:40 +0100

    Set version to alpha, use get version in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 readme = ''
 
 setup(
     long_description=readme,
     name='vip-ipykernel',
-    version='0.1.1',
+    version=get_version("./src/vip_ipykernel/__init__.py"),
     python_requires='==3.*,>=3.6.0',
     author='Robert Rosca',
     author_email='32569096+RobertRosca@users.noreply.github.com',
