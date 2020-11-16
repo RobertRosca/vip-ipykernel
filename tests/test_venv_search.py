4891d63d32b71c83c9a35da9b1f50cb7c253a75e
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2020-11-16 13:26:46 +0100

    Add tests

---
+++ tests/test_venv_search.py
@@ -0,0 +1,22 @@
+from vip_ipykernel.vip_ipykernel_launcher import venv_search
+import os
+import sys
+from pathlib import Path
+
+
+def test_venv_search(tmp_path):
+    os.chdir(tmp_path)
+
+    venv_bin = tmp_path / '.venv' / 'bin'
+    venv_bin.mkdir(parents=True)
+
+    venv_executable = venv_bin / 'python3'
+    venv_executable.touch()
+
+    assert venv_executable == venv_search()
+
+
+def test_venv_search_missing(tmp_path):
+    os.chdir(tmp_path)
+
+    assert Path(sys.executable) == venv_search()
--- tests/test_vip_ipykernel.py
+++
@@ -1,5 +0,0 @@
-from vip_ipykernel import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
